ff917a6fb154398eda0dcfc771cef579812bc72e
sandrodevv <kiknadze.s004@gmail.com>
2025-02-12 18:22:20 +0400

    Add files via upload

---
+++ store/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
+
+def product_add(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list') 
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_form.html', {'form': form})
+
+def product_update(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail', product_id=product.id)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/product_form.html', {'form': form})
+
+
+def product_delete(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'products/product_confirm_delete.html', {'product': product})
